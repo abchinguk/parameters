de3b8b95e9026e4b03e616380206befcab6983a3
abchinguk <allenetie590@gmail.com>
2016-01-31 15:48:54 +0300

    initial commit

---
+++ parameters.rb
@@ -0,0 +1,17 @@
+puts "я строка 1"
+
+puts
+
+puts("я строка 2")
+
+array = ["James Bond", "Bradd Pit", "Dr.Who", "Hous MD", "Madonna"]
+
+puts array.to_s
+
+array.pop
+
+puts array.to_s
+
+array.pop 2
+
+puts array.to_s
